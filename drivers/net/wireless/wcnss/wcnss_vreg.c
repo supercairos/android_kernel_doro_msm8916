2f73d05506c735d7f98d8c54aaaaa47cd6f6a099
Romain Caire <romain.caire@doro.com>
2015-10-01 19:23:11 +0200

    Wireless
    Change-Id: I024a8bdd5d4cc31185d5bfc6c5d21b0441741621

--- drivers/net/wireless/wcnss/wcnss_vreg.c
+++ drivers/net/wireless/wcnss/wcnss_vreg.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -49,6 +49,7 @@ static int is_power_on;
 #define WCN3620       0x5111
 #define WCN3620A      0x5112
 #define WCN3610       0x9101
+#define WCN3610V1     0x9110
 
 #define WCNSS_PMU_CFG_IRIS_XO_CFG          BIT(3)
 #define WCNSS_PMU_CFG_IRIS_XO_EN           BIT(4)
@@ -187,6 +188,7 @@ int validate_iris_chip_id(u32 reg)
 	case WCN3620:
 	case WCN3620A:
 	case WCN3610:
+	case WCN3610V1:
 		return 0;
 	default:
 		return 1;
